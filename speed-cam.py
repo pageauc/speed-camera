78ef5c62e0a82e1c0f33f623cd2a4046c03ca533
Claude Pageau <pageauc@gmail.com>
2017-02-05 19:02:24 -0600

    ver 3.5 release remove csv file header text

--- speed-cam.py
+++ speed-cam.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-version = "version 3.40"
+version = "version 3.50"
 
 """
 speed2 written by Claude Pageau pageauc@gmail.com
@@ -277,9 +277,9 @@ def log_to_file(data_to_append):
         log_file_path = baseDir + baseFileName + ".csv"
         if not os.path.exists(log_file_path):
             open( log_file_path, 'w' ).close()
-            header_text = '"YYYYMMDD","HH","MM","Speed","Unit","    Speed Photo Path            ","W","H","Area"' + "\n"
+            # header_text = '"YYYYMMDD","HH","MM","Speed","Unit","    Speed Photo Path            ","W","H","Area"' + "\n"
             f = open( log_file_path, 'ab' )
-            f.write( header_text )
+            # f.write( header_text )
             f.close()
             msgStr = "Create New Data Log File %s" % log_file_path
             show_message("log_to_file ", msgStr)
