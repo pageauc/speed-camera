61d3c933d9af9a1bfdbbd9e1b1f3cc433208eac8
Claude Pageau <pageauc@gmail.com>
2018-01-19 12:24:22 -0600

    Added plugins Readme.md

--- config.py
+++ config.py
@@ -124,7 +124,7 @@ THRESHOLD_SENSITIVITY = 20  # OpenCV setting for difference image threshold
 # -------------------
 web_server_port = 8080        # default= 8080 Web server access port eg http://192.168.1.100:8080
 web_server_root = "media"     # default= "media" webserver root path to webserver image/video sub-folders
-web_page_title = "Speed Camera"  # web page title that browser show (not displayed on web page)
+web_page_title = "SPEED-CAMERA Media"  # web page title that browser show (not displayed on web page)
 web_page_refresh_on = False   # False=Off (never)  Refresh True=On (per seconds below)
 web_page_refresh_sec = "180"  # default= "180" seconds to wait for web page refresh  seconds (three minutes)
 web_page_blank = True         # True Starts left image with a blank page until a right menu item is selected
---
+++ plugins/Readme.md
@@ -0,0 +1,10 @@
+# SPEED-CAMERA plugins
+
+## Introduction
+Plugins configure speed camera for special tasks by overlaying
+the plugin variables over the current config.py settings.
+Plugin Files can be found in the speed-camera/plugins subfolder.
+You can customize a particular plugin or create your own.   
+***IMPORTANT*** Do Not delete the `__init__.py` file since it is required in order to import plugins. 
+
+### For Details see Wiki [How to Use Plugins](https://github.com/pageauc/speed-camera/wiki/Admin-and-Settings) 
