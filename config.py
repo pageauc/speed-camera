02d95f15ff9841c3e424ce52addee4ad0a0d605d
Claude Pageau <pageauc@gmail.com>
2018-02-13 08:58:24 -0600

    Release 8.4 Minor format changes

--- config.py
+++ config.py
@@ -23,15 +23,15 @@ pluginName = "picam480"   # Specify filename in plugins subfolder without .py ex
 # ---------------------------------------
 x_left  = 150          # Default= 150 Exclude event if x less than this px position Default=25
 x_right = 490          # Default= 490 Exclude event if x greater than this px position Default=295
-y_upper = 140          # Default= 140 Exclude event if y less that this value default=100
-y_lower = 340          # Default= 340 Exclude event if y greater than this value default=175
+y_upper = 140          # Default= 140 Exclude event if y less that this value Default=100
+y_lower = 340          # Default= 340 Exclude event if y greater than this value Default=175
 
 # Display opencv windows on gui desktop
 # gui_window_on suppresses All Windows if False
 # ----------------------------------------------
 gui_window_on = False      # True= Turn On All desktop GUI openCV windows. False=Don't Show (req'd for SSH) .
 show_thresh_on = False     # Display desktop GUI openCV cropped threshold window. True=Show, False=Don't Show
-show_crop_on = False       # Same as show_thresh_on but in color. True=Show, False=Don't Show (default)
+show_crop_on = False       # Same as show_thresh_on but in color. True=Show, False=Don't Show (Default)
 
 # Display and Log settings
 # ------------------------
@@ -66,8 +66,8 @@ WEBCAM_VFLIP = False   # Default= False USB Webcam flip image vertically
 
 # Pi Camera Settings
 # ------------------
-CAMERA_WIDTH = 640     # Image stream width for opencv motion scanning default=320
-CAMERA_HEIGHT = 480    # Image stream height for opencv motion scanning  default=240
+CAMERA_WIDTH = 640     # Image stream width for opencv motion scanning Default=320
+CAMERA_HEIGHT = 480    # Image stream height for opencv motion scanning  Default=240
 CAMERA_FRAMERATE = 25  # Default= 25 Frame rate for video stream V2 picam can be higher
 CAMERA_ROTATION = 0    # Rotate camera image valid values are 0, 90, 180, 270
 CAMERA_VFLIP = True    # Flip the camera image vertically if required
@@ -77,14 +77,14 @@ CAMERA_HFLIP = True    # Flip the camera image horizontally if required
 # ---------------------
 image_path = "media/images"   # folder name to store images
 image_prefix = "speed-"       # image name prefix
-image_format = ".jpg"         # default = ".jpg"  image Formats .jpeg .png .gif .bmp
+image_format = ".jpg"         # Default = ".jpg"  image Formats .jpeg .png .gif .bmp
 image_show_motion_area = True # True= Display motion detection rectangle area on saved images
 image_filename_speed = False  # True= Prefix filename with speed value
 image_text_on = True          # True= Show Text on speed images   False= No Text on images
 image_text_bottom = True      # True= Show image text at bottom otherwise at top
 image_font_size = 15          # Default= 15 Font text height in px for text on images
 image_bigger = 1.5            # Default= 1.5 Resize saved speed image by value
-image_max_files = 0           # 0=off or specify MaxFiles to maintain then oldest are deleted  default=0 (off)
+image_max_files = 0           # 0=off or specify MaxFiles to maintain then oldest are deleted  Default=0 (off)
 
 # Optional Manage SubDir Creation by time, number of files or both
 # ----------------------------------------------------------------
@@ -93,25 +93,25 @@ imageSubDirMaxFiles = 1000    # 0=off or specify MaxFiles - Creates New dated su
 
 # Optional Save Most Recent files in recent folder
 # ------------------------------------------------
-imageRecentMax = 100        # 0=off  Maintain specified number of most recent files in motionRecentDir
-imageRecentDir = "media/recent"  # default= "media/recent"  save recent files directory path
+imageRecentMax = 100          # 0=off  Maintain specified number of most recent files in motionRecentDir
+imageRecentDir = "media/recent"  # Default= "media/recent"  save recent files directory path
 
 # Optional Manage Free Disk Space Settings
 # ----------------------------------------
-spaceTimerHrs = 24          # default= 0  0=off or specify hours frequency to perform free disk space check
-spaceFreeMB = 500           # default= 500  Target Free space in MB Required.
-spaceMediaDir = 'media/images'  # default= 'media/images'  Starting point for directory walk
-spaceFileExt  = 'jpg'       # default= 'jpg' File extension to Delete Oldest Files
+spaceTimerHrs = 24            # Default= 0  0=off or specify hours frequency to perform free disk space check
+spaceFreeMB = 500             # Default= 500  Target Free space in MB Required.
+spaceMediaDir = 'media/images'  # Default= 'media/images'  Starting point for directory walk
+spaceFileExt  = 'jpg'         # Default= 'jpg' File extension to Delete Oldest Files
 
 # OpenCV Motion Settings
 # ----------------------
-SHOW_CIRCLE = True          # True=circle in center of motion, False=rectangle
-CIRCLE_SIZE = 2             # Diameter circle in px if SHOW_CIRCLE = True
-LINE_THICKNESS = 1          # Size of lines for circle or Rectangle
-FONT_SCALE = .5             # OpenCV window text font size scaling factor default=.5 (lower is smaller)
-WINDOW_BIGGER = 1           # default=1 resize multiplier for opencv window if gui_window_on=True
-BLUR_SIZE = 10              # OpenCV setting for Gaussian difference image blur
-THRESHOLD_SENSITIVITY = 20  # OpenCV setting for difference image threshold
+SHOW_CIRCLE = True            # True=circle in center of motion, False=rectangle
+CIRCLE_SIZE = 2               # Default= 2 Diameter circle in px if SHOW_CIRCLE = True
+LINE_THICKNESS = 1            # Default= 1 Size of lines for circle or Rectangle
+FONT_SCALE = 0.5              # Default= 0.5 OpenCV window text font size scaling factor Default=.5 (lower is smaller)
+WINDOW_BIGGER = 1             # Default=1 Resize multiplier for opencv window if gui_window_on=True
+BLUR_SIZE = 10                # Default= 10 OpenCV setting for Gaussian difference image blur
+THRESHOLD_SENSITIVITY = 20    # Default= 20 OpenCV setting for difference image threshold
 
 #======================================
 #       webserver.py Settings
@@ -119,26 +119,26 @@ THRESHOLD_SENSITIVITY = 20  # OpenCV setting for difference image threshold
 
 # Web Server settings
 # -------------------
-web_server_port = 8080        # default= 8080 Web server access port eg http://192.168.1.100:8080
-web_server_root = "media"     # default= "media" webserver root path to webserver image/video sub-folders
+web_server_port = 8080        # Default= 8080 Web server access port eg http://192.168.1.100:8080
+web_server_root = "media"     # Default= "media" webserver root path to webserver image/video sub-folders
 web_page_title = "SPEED-CAMERA Media"  # web page title that browser show (not displayed on web page)
 web_page_refresh_on = False   # False=Off (never)  Refresh True=On (per seconds below)
-web_page_refresh_sec = "180"  # default= "180" seconds to wait for web page refresh  seconds (three minutes)
+web_page_refresh_sec = "180"  # Default= "180" seconds to wait for web page refresh  seconds (three minutes)
 web_page_blank = True         # True Starts left image with a blank page until a right menu item is selected
                               # False displays second list[1] item since first may be in progress
 
 # Left iFrame Image Settings
 # --------------------------
-web_image_height = "768"       # default= "768" px height of images to display in iframe
+web_image_height = "768"       # Default= "768" px height of images to display in iframe
 web_iframe_width_usage = "70%" # Left Pane - Sets % of total screen width allowed for iframe. Rest for right list
 web_iframe_width = "100%"      # Desired frame width to display images. can be eg percent "80%" or px "1280"
 web_iframe_height = "100%"     # Desired frame height to display images. Scroll bars if image larger (percent or px)
 
 # Right Side Files List
 # ---------------------
-web_max_list_entries = 0         # 0 = All or Specify Max right side file entries to show (must be > 1)
-web_list_height = web_image_height  # Right List - side menu height in px (link selection)
-web_list_by_datetime = True      # True=datetime False=filename
-web_list_sort_descending = True  # reverse sort order (filename or datetime per web_list_by_datetime setting
+web_max_list_entries = 0           # 0 = All or Specify Max right side file entries to show (must be > 1)
+web_list_height = web_image_height # Right List - side menu height in px (link selection)
+web_list_by_datetime = True        # True=datetime False=filename
+web_list_sort_descending = True    # reverse sort order (filename or datetime per web_list_by_datetime setting
 
 # ---------------------------------------------- End of User Variables -----------------------------------------------------
