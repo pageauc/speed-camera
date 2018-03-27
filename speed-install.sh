c3e24203c9129031f63b9c5ff22ff2d342393fb1
Claude Pageau <pageauc@gmail.com>
2018-03-26 20:10:36 -0500

    update rclone to ver 1.40

--- speed-install.sh
+++ speed-install.sh
@@ -4,7 +4,7 @@
 ver="8.0"
 SPEED_DIR='speed-camera'  # Default folder install location
 # Make sure ver below matches latest rclone ver on https://downloads.rclone.org/rclone-current-linux-arm.zip
-rclone_cur_ver="rclone v1.39"
+rclone_cur_ver="rclone v1.40"
 
 cd ~
 is_upgrade=false
