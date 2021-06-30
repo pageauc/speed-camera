5057b7d7159f6897eaf90aa2c574181bea484b8d
Claude Pageau <pageauc@gmail.com>
2021-06-30 12:21:00 -0400

    release 11.08 Adde download of user_motion_code.py for initial install

--- speed-install.sh
+++ speed-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # speed-install.sh script written by Claude Pageau 1-Jul-2016
 
-ver="11.0"
+ver="11.08"
 SPEED_DIR='speed-camera'  # Default folder install location
 # Make sure ver below matches latest rclone ver on https://downloads.rclone.org/rclone-current-linux-arm.zip
 rclone_cur_ver="rclone v1.55.1"
@@ -36,7 +36,7 @@ else
     speedFiles=("config.py" "menubox.sh" "speed-cam.py" "sql_speed_gt.py" \
 "speed-cam.sh" "search-speed.py" "search_config.py" "Readme.md" "makehtml.py" "webserver.py" \
 "webserver.sh" "webserver3.py" "rclone-security-sync-recent.sh" "remote-run.sh" "watch-app.sh" \
-"alpr-speed.py" "sql-make-graph-count-totals.py" "sql-make-graph-speed-ave.py")
+"alpr-speed.py" "sql-make-graph-count-totals.py" "sql-make-graph-speed-ave.py" "user_motion_code.py")
 fi
 
 for fname in "${speedFiles[@]}" ; do
@@ -49,6 +49,7 @@ for fname in "${speedFiles[@]}" ; do
         fi
     fi
 done
+wget -q --show-progress -nc https://raw.github.com/pageauc/speed-camera/master/user_motion_code.py
 wget -O media/webserver.txt -q --show-progress https://raw.github.com/pageauc/speed-camera/master/webserver.txt
 wget -O config.py.new -q --show-progress https://raw.github.com/pageauc/speed-camera/master/config.py
 
