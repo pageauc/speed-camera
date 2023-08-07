879c1d440c20371d727e1856c8428fc1837f779c
Claude Pageau <pageauc@gmail.com>
2023-08-07 06:56:45 -0400

    Update speed-install.sh

--- speed-install.sh
+++ speed-install.sh
@@ -4,7 +4,7 @@
 ver="11.08"
 SPEED_DIR='speed-camera'  # Default folder install location
 # Make sure ver below matches latest rclone ver on https://downloads.rclone.org/rclone-current-linux-arm.zip
-rclone_cur_ver="rclone v1.55.1"
+rclone_cur_ver="rclone v1.63.1"
 
 cd ~
 is_upgrade=false
