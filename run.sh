3f9e3800f9f8e0817deeb03620fb4a9299f2610d
Claude Pageau <pageauc@gmail.com>
2024-03-18 08:23:19 -0500

    Update to run as non root user

--- run.sh
+++ run.sh
@@ -5,20 +5,22 @@ version="2.0"
 programs="speed-cam.py webserver.py"
 params="start, stop, restart, status, install, uninstall"
 
-
 echo "$0 ver $version  written by Claude Pageau"
 echo "Control $programs"
 echo ""
 
-
 if [ "$1" = "start" ]; then
     echo "sudo supervisorctl start speed-cam speed-web"
+    sudo supervisorctl reread
+    sudo supervisorctl update
     sudo supervisorctl start speed-cam speed-web
     echo "Wait 10 seconds for supervisor services to start $programs"
     sleep 10
 elif [ "$1" = "stop" ]; then
     echo "STOP: sudo supervisorctl stop $programs"
     sudo supervisorctl stop speed-cam speed-web
+    sudo supervisorctl status all
+    exit 0
 elif [ "$1" = "status" ]; then
     echo "STATUS: sudo supervisorctl status all"
     echo "supervisorctl status for $programs"
@@ -32,18 +34,18 @@ elif [ "$1" = "install" ]; then
         echo "INFO  - Done Install supervisorctl for $programs"
     else
         echo "WARN  - suprvisorctl for $programs Already Installed."
+        exit 0
     fi
-    exit 0
 elif [ "$1" = "uninstall" ]; then
     echo "Uninstall supervisorctl for $programs"
     sudo supervisorctl stop speed-cam speed-web
     sudo rm /etc/supervisor/conf.d/speed-cam.conf /etc/supervisor/conf.d/speed-web.conf
     exit 0
-
 elif [ "$1" = "upgrade" ]; then
     echo "Upgrade program files from github repo"
     echo " curl -L https://raw.github.com/pageauc/speed-camera/master/speed-install.sh | bash "
     curl -L https://raw.github.com/pageauc/speed-camera/master/speed-install.sh | bash
+    echo "Upgrade Finished"
     exit 0
 else
    echo "Usage: ./run.sh [Option]"
@@ -58,14 +60,12 @@ else
    echo "  help         Display Usage message and Status"
    echo ""
    echo "Example:  ./run.sh status"
+   exit 0
 fi
 
 echo ""
 myip1=$(hostname -I | cut -d " " -f 1)
 myip2=$(hostname -I | cut -d " " -f 2)
-echo "sudo supervisorctl status all"
-echo "supervisorctl status for $programs" 
-sudo supervisorctl status all
 echo ""
 echo " To Access RUNNING speed-camera websever interface"
 echo "    Type or copy/paste url link below"
