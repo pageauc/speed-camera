b6e4ca223e47c9937464bda0c9b2a53e16b5fca3
Claude Pageau <pageauc@gmail.com>
2025-03-09 12:44:57 -0500

    Update speed-install.sh

--- source/speed-install.sh
+++ source/speed-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # speed-install.sh script written by Claude Pageau 1-Jul-2016
 
-ver="13.2"
+ver="13.3"
 SPEED_DIR='speed-camera'  # Default folder install location
 # Make sure ver below matches latest rclone ver on https://downloads.rclone.org/rclone-current-linux-arm.zip
 
